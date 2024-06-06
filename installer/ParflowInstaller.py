bd0b947fc8db1d224cd0ba06658a9b144b8b24ed
westb2 <west.bd@gmail.com>
2024-06-06 15:09:16 -0700

    Updating system package manager workflow

--- installer/ParflowInstaller.py
+++ installer/ParflowInstaller.py
@@ -135,8 +135,8 @@ class ParflowInstaller:
         create_directory(config.INSTALLATION_ROOT)
         os.chdir(config.INSTALLATION_ROOT)
         # first install everything we can via our system package manager
-        # for package in config.REQUIRED_PACKAGES[self.system_package_manager.package_manager]:
-        #     self.system_package_manager.install_package(package)
+        for package in config.REQUIRED_PACKAGES[self.system_package_manager.package_manager]:
+            self.system_package_manager.install_package(package)
         # next install the packages we need to build and configure from source.
         # The order of these matters!
         self.install_hdf5()
