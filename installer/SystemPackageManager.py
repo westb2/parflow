a14e9707639e8e491807833e75358bde4368003b
westb2 <west.bd@gmail.com>
2024-06-06 13:24:42 -0700

    Updating system package manager workflow

--- installer/SystemPackageManager.py
+++ installer/SystemPackageManager.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from utils import *
 
 
@@ -6,8 +7,24 @@ class SystemPackageManager:
     def __init__(self, package_manager):
         if package_manager == "AUTO_CONFIGURED":
             # make this instantiate a HomebrewManager child class
-            self.package_manager = "brew"
-            self.automatic_yes_flag = ""
+            self.package_manager = self.determine_package_manager
+            print(f"We determined the package manager to be {self.package_manager}\n")
+            if self.package_manager == "brew":
+                self.automatic_yes_flag = ""
+            else:
+                self.automatic_yes_flag = "-y"
+
+    def determine_package_manager(self):
+        potential_package_managers = ["brew", "yum", "apt-get"]
+        for package_manager in potential_package_managers:
+            package_manager_location = subprocess.check_output(["which",  f"{package_manager}"])
+            if package_manager_location != "":
+                    return package_manager
+        raise Exception(
+        '''Could not detect your systems package manager. Try going to config.py and
+        either setting it manually, but if it is not one of brew, yum, or apt-get this tool
+        may not work for you'''
+    )
 
 
     def get_homebrew_package_location(self, package):
